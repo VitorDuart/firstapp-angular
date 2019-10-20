83e7d26e10b414f6446135b2f7cfaa685437e1a7
VitorDuart <victorduart366@gmail.com>
2019-10-20 14:07:18 -0300

    Refatorado algumas coisas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
-import { CartService } from './cart.service';
+import { CartService } from './services/cart.service';
 import { CartComponent } from './cart/cart.component';
 import { ShippingComponent } from './shipping/shipping.component';
 
@@ -41,7 +41,7 @@ import { ShippingComponent } from './shipping/shipping.component';
     ShippingComponent
   ],
   bootstrap: [ AppComponent ],
-  providers: [CartService]
+  providers: [ CartService ]
 })
 export class AppModule { }
 
